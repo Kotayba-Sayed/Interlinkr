b9ae485902f254db3efb15765e73e7ff00631905
Couziii <couragerasanen@gmail.com>
2024-05-21 22:54:33 +0200

    ProtectedRoute update

--- frontend/src/components/routes/ProtectedRoute.js
+++ frontend/src/components/routes/ProtectedRoute.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
+import { useNavigate, Outlet } from 'react-router-dom';
 import { useUsername } from '../context/UserContext';
+import { useAuth } from '../context/AuthProvider';
 
 const ProtectedRoute = () => {
-  const { username } = useUsername();
-  console.log(username)
-  console.log(username === 'admin')
-  return username === 'admin' ? <Outlet /> : <Navigate to="/login" />;
+    const { username } = useUsername();
+    const { setToken } = useAuth();
+    const { setUsername } = useUsername();
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        setToken();
+        setUsername();
+        navigate("/", { replace: true });
+    };
+
+    console.log(username)
+    console.log(username === 'admin')
+    return (
+        username === 'admin' ? <Outlet /> : handleLogout()
+    );
 
 };
 
